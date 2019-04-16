c1eac226e1d9c8224b24b00b00d1205274afd818
AkbarAminov <akbar.aminov.97@gmail.com>
2019-04-16 23:13:43 +0500

    changed navbar and stylized

--- public/js/app.js
+++ public/js/app.js
@@ -21929,8 +21929,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/iskandar/Documents/Sites/babycheeks/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/iskandar/Documents/Sites/babycheeks/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\babycheeks\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\babycheeks\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
