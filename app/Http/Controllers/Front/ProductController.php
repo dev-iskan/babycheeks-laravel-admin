bd8c3bbe2263c83714a1d86feba7b23f38c3139a
dev-iskan <successfuliskan@gmail.com>
2019-04-23 16:14:01 +0500

    added real categories to main page

--- app/Http/Controllers/Front/ProductController.php
+++ app/Http/Controllers/Front/ProductController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\Front;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Product $product)
     {
-      return view('pages.product');
+      return view('pages.product', compact('product'));
     }
 }
---
+++ resources/views/pages/partials/mainpage_category.blade.php
@@ -0,0 +1,14 @@
+<div class="tile is-child box">
+    <article class="tile is-child">
+        <!-- Put any content you want -->
+        <figure class="image is-4by3">
+          {{$category->getFirstMedia('categories')}}
+        </figure>
+        <div class="d-flex flex-column-nowrap">
+          <p class="is-size-3 has-text-weight-semibold">{{$category->name}}</p>
+          <div>
+            {!! $category->description !!}
+          </div>
+        </div>
+      </article>
+</div>
